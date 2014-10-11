54148c6c3942e619ace8225e3952040df7bca12b
moonloverslover <jby1985@gmail.com>
2014-10-11 17:25:09 -0400

    MongoEngine connection built

--- routes.py
+++ routes.py
@@ -1,39 +1,34 @@
 from flask import *
 from functools import wraps
-import pymongo
+
 
 from flask.ext.mongoengine import MongoEngine
-from mongoengine import connect
-
-
-
-
 
 app = Flask(__name__)
-app.config.from_object(__name__)
-app.config['DATABASE']
 
-"""
-app.config['MONGODB_SETTINGS'] = {
-    'db': 'tasks',
-    'host': 'localhost',
-    'port': 27017
-}
-connect('tasks')
+app.config['SECRET_KEY'] = 'super-secret'
+
+# MongoDB Config
+app.config['MONGODB_DB'] = 'tasksDB'
+app.config['MONGODB_HOST'] = 'localhost'
+app.config['MONGODB_PORT'] = 27017
 
 db = MongoEngine(app)
 
-"""
-#db.createCollection(alltasks)
-#task = {'name':'CGM','date':'10/15/2014','priority':'1','status':1}
-#db.alltasks.insert(task)
-
-app.secret_key = 'my precious'
 
 
+class Task(db.Document):
+	name = db.StringField(max_length=50, required=True)
+	due_date = db.StringField(max_length=15, required=True)
+	priority = db.StringField(max_length=2, required=True)
+	status = db.IntField(required=True)
 
-def connect_db():
-	return pymongo.MongoClient()
+
+# Create a user to test with
+#@app.before_first_request
+#def dummy_task():
+#	Task(name='Dummy', due_date='10/10/2999',priority='1',status=0).save()
+
 
 @app.route('/',methods=['GET','POST'])
 def home():
@@ -60,11 +55,11 @@ def login_required(test):
 @app.route('/tasks')
 @login_required
 def tasks():
-	task = db.alltasks.find_one()
+	opentasks = Task.objects(status=1)
+	closedtasks = Task.objects(status=0)
+	return render_template('tasks.html',opentasks=opentasks,closedtasks=closedtasks)
 
-	return render_template('tasks.html',task=task)
-
-@app.route('/tasks')
+@app.route('/tasks',methods=['GET','POST'])
 @login_required	
 def new_task():
 	name = request.form['name']
@@ -73,20 +68,49 @@ def new_task():
 	if not name and not date and not priority:
 		flash('You forgot the task name, due date and priority!')
 		return redirect(url_for('tasks'))
+	elif not name and not date:
+		flash('You forgot the name and date!')
+		return redirect(url_for('tasks'))
+	elif not date and not priority:
+		flash('You forgot the date and priority!')
+		return redirect(url_for('tasks'))
+	elif not name and not priority:
+		flash('You forgot the name and priority!')
+		return redirect(url_for('tasks'))
+	elif not name:
+		flash('You forgot the name!')
+		return redirect(url_for('tasks'))
+	elif not date:
+		flash('You forgot the date!')
+		return redirect(url_for('tasks'))
+	elif not priority:
+		flash('You forgot the priority!')
+		return redirect(url_for('tasks'))					
 	else:
-		task = {"name":name,"date":date,"priority":priority,"status":1}
-		db.alltasks.insert(task)
-		flash('New entry was successfully posted')	
+		Task(name=name, due_date=date,priority=priority,status=1).save()
+		flash('New entry was successfully posted.')	
 		return redirect(url_for('tasks'))	
-	
+
+@app.route('/delete/<task_id>')
+@login_required	
+def delete_task(task_id):
+	Task.objects(id=task_id).delete()
+	flash('The task was deleted!')
+	return redirect(url_for('tasks'))	
+
+@app.route('/complete/<task_id>')
+@login_required	
+def mark_complete(task_id):
+	task = Task.objects(id=task_id).update(set__status=0)
+	flash('The task was marked as complete!')
+	return redirect(url_for('tasks'))
+
+
 
 @app.route('/logout')
 def logout():
 	session.pop('logged_in',None)
 	return redirect(url_for('home'))	
-
-
-
 
 
 
